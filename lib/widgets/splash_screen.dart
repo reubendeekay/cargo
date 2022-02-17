e5b765897be88241efae7485d1a25719cea04d42
reubendeekay <mrreubenyt@gmail.com>
2022-02-17 11:04:37 +0300

    Database Init

--- lib/widgets/splash_screen.dart
+++ lib/widgets/splash_screen.dart
@@ -1,4 +1,7 @@
+import 'package:cargo/screens/agent/dashboard.dart';
+import 'package:cargo/screens/home/homepage.dart';
 import 'package:flutter/material.dart';
+import 'package:get/route_manager.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -14,19 +17,9 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Future.delayed(Duration.zero, () async {
-      await startAnimation();
+    Future.delayed(const Duration(seconds: 2), () async {
+      await Get.to(() => const Homepage());
     });
-  }
-
-  Future<void> startAnimation() async {
-    for (int i = 0; i < 25; i++) {
-      await Future.delayed(const Duration(milliseconds: 200), () {
-        setState(() {
-          heigth = heigth * 4;
-        });
-      });
-    }
   }
 
   @override
@@ -35,14 +28,21 @@ class _SplashScreenState extends State<SplashScreen> {
     return Scaffold(
       body: Stack(
         children: [
-          Center(
-              child: CircleAvatar(
-            radius: heigth,
-            backgroundColor: Colors.white,
-            backgroundImage: const NetworkImage(
-              'https://images.unsplash.com/photo-1613690399151-65ea69478674?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=873&q=80',
+          const Center(
+            child: CircleAvatar(
+              radius: 50,
+              backgroundColor: Color.fromRGBO(255, 255, 255, 1),
+              backgroundImage: AssetImage('assets/images/logo.png'),
             ),
-          )),
+          ),
+          Container(
+              height: heigth,
+              width: size.width,
+              color: const Color.fromRGBO(255, 255, 255, 1),
+              child: Image.network(
+                'https://images.unsplash.com/photo-1613690399151-65ea69478674?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=873&q=80',
+                fit: BoxFit.cover,
+              ))
         ],
       ),
     );
